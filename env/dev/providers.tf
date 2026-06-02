d268109a44b02f4ba1d8a54c45d4bdd6508cf023
Aquib Jaleel <aquibjaleel@gmail.com>
2026-06-02 11:39:15 -0400

    added backend s3 and state locking

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -6,14 +6,16 @@ terraform {
       version = "~> 5.0"
     }
   }
-  # Optional: Uncomment this later once you have an S3 bucket to store your state file
-  # backend "s3" {
-  #   bucket         = "your-name-terraform-state-bucket"
-  #   key            = "dev/infrastructure.tfstate"
-  #   region         = "us-east-1"
-  # }
+  #Optional: Uncomment this later once you have an S3 bucket to store your state file
+backend "s3" {
+  bucket         = "skillslab-tfstate-bucket-aquib-2026"
+  key            = "dev/infrastructure.tfstate"
+  region         = "us-east-1"
+}
+
 }
 
 provider "aws" {
   region = "us-east-1"
 }
+
